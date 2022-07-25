41d8663eb05fd3ef5532194f4d3bbb119c8eb7f8
rita <rita@pop-os.localdomain>
2022-07-25 13:19:46 +0200

    firstcommit

---
+++ resources/views/radio.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>live-radio</title>
+</head>
+<body>
+    <h3>welcome to our live radio</h3>
+</body>
+</html>
